381bc07a40b390ac3a38c31c450f6d9e84c58187
Sava Chankov <sava@tutuf.com>
2010-01-21 21:47:42 +0200

    Fixed change_table for activerecord < 2.3.5

--- lib/connection_adapters/postgresql_adapter.rb
+++ lib/connection_adapters/postgresql_adapter.rb
@@ -3,7 +3,8 @@ module Fkey #:nodoc:
     def add_foreign_key(to_table, column, options={})
       to_table = options[:references] if options[:references]
       res = "FOREIGN KEY(#{quote_column_name(column)}) REFERENCES #{quote_table_name(to_table)}"
-      res += "(#{options[:referenced_column]})" if options[:referenced_column]
+      referenced_column = options[:referenced_column] || options[:primary_key]
+      res += "(#{referenced_column})" if referenced_column
       res
     end
     
@@ -12,7 +13,8 @@ module Fkey #:nodoc:
     def add_column_with_foreign_key(table_name, column_name, type, options = {})
       default = options[:default]
       notnull = options[:null] == false
-      foreign_key = "REFERENCES #{quote_table_name(options[:references])}(#{quote_column_name(options[:referenced_column] || primary_key(options[:references]))})"
+      foreign_key = "REFERENCES #{quote_table_name(options[:references])}"
+      foreign_key +="(#{quote_column_name(options[:referenced_column] || options[:primary_key])})" if options[:referenced_column] || options[:primary_key]
       
       # Add the column.
       execute("ALTER TABLE #{quote_table_name(table_name)} ADD COLUMN #{quote_column_name(column_name)} #{type_to_sql(type, options[:limit], options[:precision], options[:scale])} #{foreign_key}")
