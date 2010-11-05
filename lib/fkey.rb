ec505eb9eb065291cfe5a383030a3fadbfd07367
Evgeni Dzhelyov <evgeni.dzhelyov@gmail.com>
2010-11-05 16:44:08 +0200

    Port the plugin to Rails 3.

--- lib/fkey.rb
+++ lib/fkey.rb
@@ -1,18 +1,27 @@
 require 'connection_adapters/abstract/schema_definitions'
 
-module ActiveRecord
-  module ConnectionAdapters    
-    TableDefinition.class_eval{include Fkey::TableDefinition}
-    Table.class_eval{include Fkey::Table}
+module Fakey
+  class << self
+    def load
+      ActiveRecord::ConnectionAdapters::TableDefinition.class_eval do
+        include Fkey::TableDefinition
+      end
+      ActiveRecord::ConnectionAdapters::Table.class_eval do
+        include Fkey::Table
+      end
+      if defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+        require 'connection_adapters/postgresql_adapter'
+        ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.class_eval{include Fkey::PostgreSQLAdapter}
+      elsif defined?(ActiveRecord::ConnectionAdapters::MysqlAdapter)
+        require 'connection_adapters/mysql_adapter'
+        ActiveRecord::ConnectionAdapters::MysqlAdapter.class_eval{include Fkey::MysqlAdapter}
+      else
+        raise "Only PostgreSQL and MySQL are currently supported by the fakey plugin."
+      end
+    end
   end
 end
 
-if defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
-  require 'connection_adapters/postgresql_adapter'
-  ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.class_eval{include Fkey::PostgreSQLAdapter}
-elsif defined?(ActiveRecord::ConnectionAdapters::MysqlAdapter)
-  require 'connection_adapters/mysql_adapter'
-  ActiveRecord::ConnectionAdapters::MysqlAdapter.class_eval{include Fkey::MysqlAdapter}
-else
-  raise "Only PostgreSQL and MySQL are currently supported by the fakey plugin."
+ActiveSupport.on_load :active_record do
+  Fakey.load
 end
